f8a93746ffac33b77dfe838891d4dc64cd1b2ab6
henry <hhuiwen@cn.ibm.com>
2014-04-26 22:52:16 +0800

    refactored the structure

---
+++ neo4j.user/src/main/java/me/huiwen/example/neo4j/user/node/User.java
@@ -0,0 +1,30 @@
+package me.huiwen.example.neo4j.user.node;
+
+/**
+ * Data object for mapping of database table user
+ * 
+ * @author Charlie Li (Li Cheng Hong)
+ * @version 0.1
+ * 
+ */
+public class User {
+	private static final long serialVersionUID = -386635962736809811L;
+	private String name;
+
+	public User() {
+	}
+
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
